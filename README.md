0da0fd84396a3e350472582ce175ea4cf622091f
rogersm92 <roger.serra92@gmail.com>
2021-03-26 13:26:27 +0100

    starting the real project
    I'll build a notes app uding Node Js and MongoDB

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # web_development
 Trying Javascript with Node Js
+I'll create a Notes App using Node Js and MongoDB
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
         // this is a comment in Js.
         // First Js. code
         console.log("Hello World");
+        
